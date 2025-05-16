80da199554f72102acc385554eb677f2976de47f
twidmer <twidmer@nvidia.com>
2025-05-16 09:15:25 +0200

    Move functions step 1

--- mujoco_warp/_src/collision_convex.py
+++ mujoco_warp/_src/collision_convex.py
@@ -30,77 +30,39 @@ from .types import GeomType
 from .types import Model
 from .types import vec5
 
+from .convex_collision_newton import *
+wp.clear_kernel_cache()
+
+
+
 # TODO(team): improve compile time to enable backward pass
 wp.config.enable_backward = False
 
-FLOAT_MIN = -1e30
-FLOAT_MAX = 1e30
-EPS_BEST_COUNT = 12
-MULTI_CONTACT_COUNT = 4
-MULTI_POLYGON_COUNT = 8
-MULTI_TILT_ANGLE = 1.0
+# FLOAT_MIN = -1e30
+# FLOAT_MAX = 1e30
+# EPS_BEST_COUNT = 12
+# MULTI_CONTACT_COUNT = 4
+# MULTI_POLYGON_COUNT = 8
+# MULTI_TILT_ANGLE = 1.0
 
-matc3 = wp.types.matrix(shape=(EPS_BEST_COUNT, 3), dtype=float)
-vecc3 = wp.types.vector(EPS_BEST_COUNT * 3, dtype=float)
+# matc3 = wp.types.matrix(shape=(EPS_BEST_COUNT, 3), dtype=float)
+# vecc3 = wp.types.vector(EPS_BEST_COUNT * 3, dtype=float)
 
-# Matrix definition for the `tris` scratch space which is used to store the
-# triangles of the polytope. Note that the first dimension is 2, as we need
-# to store the previous and current polytope. But since Warp doesn't support
-# 3D matrices yet, we use 2 * 3 * EPS_BEST_COUNT as the first dimension.
-TRIS_DIM = 3 * EPS_BEST_COUNT
-mat2c3 = wp.types.matrix(shape=(2 * TRIS_DIM, 3), dtype=float)
-mat3p = wp.types.matrix(shape=(MULTI_POLYGON_COUNT, 3), dtype=float)
-mat3c = wp.types.matrix(shape=(MULTI_CONTACT_COUNT, 3), dtype=float)
-mat43 = wp.types.matrix(shape=(4, 3), dtype=float)
+# # Matrix definition for the `tris` scratch space which is used to store the
+# # triangles of the polytope. Note that the first dimension is 2, as we need
+# # to store the previous and current polytope. But since Warp doesn't support
+# # 3D matrices yet, we use 2 * 3 * EPS_BEST_COUNT as the first dimension.
+# TRIS_DIM = 3 * EPS_BEST_COUNT
+# mat2c3 = wp.types.matrix(shape=(2 * TRIS_DIM, 3), dtype=float)
+# mat3p = wp.types.matrix(shape=(MULTI_POLYGON_COUNT, 3), dtype=float)
+# mat3c = wp.types.matrix(shape=(MULTI_CONTACT_COUNT, 3), dtype=float)
+# mat43 = wp.types.matrix(shape=(4, 3), dtype=float)
 
-vec6 = wp.types.vector(6, dtype=int)
-VECI1 = vec6(0, 0, 0, 1, 1, 2)
-VECI2 = vec6(1, 2, 3, 2, 3, 3)
+# vec6 = wp.types.vector(6, dtype=int)
+# VECI1 = vec6(0, 0, 0, 1, 1, 2)
+# VECI2 = vec6(1, 2, 3, 2, 3, 3)
 
 
-@wp.func
-def _gjk_support_geom(geom: Geom, geomtype: int, dir: wp.vec3, verts: wp.array(dtype=wp.vec3)):
-  local_dir = wp.transpose(geom.rot) @ dir
-  if geomtype == int(GeomType.SPHERE.value):
-    support_pt = geom.pos + geom.size[0] * dir
-  elif geomtype == int(GeomType.BOX.value):
-    res = wp.cw_mul(wp.sign(local_dir), geom.size)
-    support_pt = geom.rot @ res + geom.pos
-  elif geomtype == int(GeomType.CAPSULE.value):
-    res = local_dir * geom.size[0]
-    # add cylinder contribution
-    res[2] += wp.sign(local_dir[2]) * geom.size[1]
-    support_pt = geom.rot @ res + geom.pos
-  elif geomtype == int(GeomType.ELLIPSOID.value):
-    res = wp.cw_mul(local_dir, geom.size)
-    res = wp.normalize(res)
-    # transform to ellipsoid
-    res = wp.cw_mul(res, geom.size)
-    support_pt = geom.rot @ res + geom.pos
-  elif geomtype == int(GeomType.CYLINDER.value):
-    res = wp.vec3(0.0, 0.0, 0.0)
-    # set result in XY plane: support on circle
-    d = wp.sqrt(wp.dot(local_dir, local_dir))
-    if d > MJ_MINVAL:
-      scl = geom.size[0] / d
-      res[0] = local_dir[0] * scl
-      res[1] = local_dir[1] * scl
-    # set result in Z direction
-    res[2] = wp.sign(local_dir[2]) * geom.size[1]
-    support_pt = geom.rot @ res + geom.pos
-  elif geomtype == int(GeomType.MESH.value):
-    max_dist = float(FLOAT_MIN)
-    # exhaustive search over all vertices
-    # TODO(team): consider hill-climb over graph data
-    for i in range(geom.vertnum):
-      vert = verts[geom.vertadr + i]
-      dist = wp.dot(vert, local_dir)
-      if dist > max_dist:
-        max_dist = dist
-        support_pt = vert
-    support_pt = geom.rot @ support_pt + geom.pos
-
-  return wp.dot(support_pt, dir), support_pt
 
 
 @wp.func
@@ -117,8 +79,8 @@ def _gjk_support(
   # Negative distance means objects are not intersecting along direction `dir`.
   # Positive distance means objects are intersecting along the given direction `dir`.
 
-  dist1, s1 = _gjk_support_geom(geom1, geomtype1, dir, verts)
-  dist2, s2 = _gjk_support_geom(geom2, geomtype2, -dir, verts)
+  dist1, s1 = gjk_support_geom(geom1, geomtype1, dir, verts)
+  dist2, s2 = gjk_support_geom(geom2, geomtype2, -dir, verts)
 
   support_pt = s1 - s2
   return dist1 + dist2, support_pt
@@ -505,7 +467,7 @@ def _gjk_epa_pipeline(
         mat8 * normal[0] + mat9 * normal[1] + mat10 * normal[2],
       )
 
-      _, p = _gjk_support_geom(geom1, geomtype1, n, mesh_vert)
+      _, p = gjk_support_geom(geom1, geomtype1, n, mesh_vert)
       v1[v1count] = wp.vec3(wp.dot(p, dir), wp.dot(p, dir2), wp.dot(p, normal))
 
       if i == 0:
@@ -514,7 +476,7 @@ def _gjk_epa_pipeline(
         v1count += 1
 
       n = -n
-      _, p = _gjk_support_geom(geom2, geomtype2, n, mesh_vert)
+      _, p = gjk_support_geom(geom2, geomtype2, n, mesh_vert)
       v2[v2count] = wp.vec3(wp.dot(p, dir), wp.dot(p, dir2), wp.dot(p, normal))
 
       if i == 0:
--- mujoco_warp/_src/convex_collision_newton.py
+++ mujoco_warp/_src/convex_collision_newton.py
@@ -1,3 +1,27 @@
+import warp as wp
+
+
+
+wp.config.enable_backward = False
+
+
+
+
+
+from .collision_primitive import Geom
+from .types import MJ_MINVAL
+from .types import GeomType
+
+
+
+
+
+
+
+
+
+
+
 FLOAT_MIN = -1e30
 FLOAT_MAX = 1e30
 EPS_BEST_COUNT = 12
@@ -21,3 +45,50 @@ mat43 = wp.types.matrix(shape=(4, 3), dtype=float)
 vec6 = wp.types.vector(6, dtype=int)
 VECI1 = vec6(0, 0, 0, 1, 1, 2)
 VECI2 = vec6(1, 2, 3, 2, 3, 3)
+
+
+
+
+@wp.func
+def gjk_support_geom(geom: Geom, geomtype: int, dir: wp.vec3, verts: wp.array(dtype=wp.vec3)):
+  local_dir = wp.transpose(geom.rot) @ dir
+  if geomtype == int(GeomType.SPHERE.value):
+    support_pt = geom.pos + geom.size[0] * dir
+  elif geomtype == int(GeomType.BOX.value):
+    res = wp.cw_mul(wp.sign(local_dir), geom.size)
+    support_pt = geom.rot @ res + geom.pos
+  elif geomtype == int(GeomType.CAPSULE.value):
+    res = local_dir * geom.size[0]
+    # add cylinder contribution
+    res[2] += wp.sign(local_dir[2]) * geom.size[1]
+    support_pt = geom.rot @ res + geom.pos
+  elif geomtype == int(GeomType.ELLIPSOID.value):
+    res = wp.cw_mul(local_dir, geom.size)
+    res = wp.normalize(res)
+    # transform to ellipsoid
+    res = wp.cw_mul(res, geom.size)
+    support_pt = geom.rot @ res + geom.pos
+  elif geomtype == int(GeomType.CYLINDER.value):
+    res = wp.vec3(0.0, 0.0, 0.0)
+    # set result in XY plane: support on circle
+    d = wp.sqrt(wp.dot(local_dir, local_dir))
+    if d > MJ_MINVAL:
+      scl = geom.size[0] / d
+      res[0] = local_dir[0] * scl
+      res[1] = local_dir[1] * scl
+    # set result in Z direction
+    res[2] = wp.sign(local_dir[2]) * geom.size[1]
+    support_pt = geom.rot @ res + geom.pos
+  elif geomtype == int(GeomType.MESH.value):
+    max_dist = float(FLOAT_MIN)
+    # exhaustive search over all vertices
+    # TODO(team): consider hill-climb over graph data
+    for i in range(geom.vertnum):
+      vert = verts[geom.vertadr + i]
+      dist = wp.dot(vert, local_dir)
+      if dist > max_dist:
+        max_dist = dist
+        support_pt = vert
+    support_pt = geom.rot @ support_pt + geom.pos
+
+  return wp.dot(support_pt, dir), support_pt
