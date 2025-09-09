5f54332b4eac1c30acdda7c7694065490ce117c9
twidmer <twidmer@nvidia.com>
2025-09-09 16:40:55 +0200

    Attempt to fix CI issues

--- mujoco_warp/_src/collision_primitive.py
+++ mujoco_warp/_src/collision_primitive.py
@@ -13,8 +13,6 @@
 # limitations under the License.
 # ==============================================================================
 
-from typing import Tuple
-
 import warp as wp
 from newton.geometry import collide_box_box as box_box
 from newton.geometry import collide_capsule_box as capsule_box
@@ -46,9 +44,7 @@ wp.set_module_options({"enable_backward": False})
 
 _HUGE_VAL = 1e6
 
-
-class mat43f(wp.types.matrix(shape=(4, 3), dtype=wp.float32)):
-  pass
+_mat43f = wp.types.matrix((4, 3), wp.float32)
 
 
 @wp.struct
@@ -150,7 +146,9 @@ def _geom(
 
 
 @wp.func
-def plane_convex(plane_normal: wp.vec3, plane_pos: wp.vec3, convex: Geom) -> Tuple[wp.vec4, mat43f, wp.vec3]:
+def plane_convex(
+  plane_normal: wp.vec3, plane_pos: wp.vec3, convex: Geom
+) -> tuple[wp.vec4, wp.types.matrix((4, 3), wp.float32), wp.vec3]:
   """Core contact geometry calculation for plane-convex collision.
 
   Args:
@@ -166,7 +164,7 @@ def plane_convex(plane_normal: wp.vec3, plane_pos: wp.vec3, convex: Geom) -> Tup
   """
 
   contact_dist = wp.vec4(wp.inf)
-  contact_pos = mat43f()
+  contact_pos = _mat43f()
   contact_count = int(0)
 
   # get points in the convex frame
